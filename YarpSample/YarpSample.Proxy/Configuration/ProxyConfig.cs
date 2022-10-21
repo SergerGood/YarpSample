f2910791333385fc454487c643f0e254143be014
SergerGood <serzher@inbox.ru>
2022-10-21 16:35:39 +0300

    Transforms

--- YarpSample/YarpSample.Proxy/Configuration/ProxyConfig.cs
+++ YarpSample/YarpSample.Proxy/Configuration/ProxyConfig.cs
@@ -1,41 +1,46 @@
 ﻿using Microsoft.Extensions.Primitives;
 using Yarp.ReverseProxy.Configuration;
 using Yarp.ReverseProxy.LoadBalancing;
+using Yarp.ReverseProxy.Transforms;
 
 namespace YarpSample.Proxy.Configuration;
 
 public class ProxyConfig : IProxyConfig
 {
     private static readonly CancellationTokenSource cts = new();
-    private static readonly string clusterId = "CustomerCluster";
+    private static readonly string clusterId = "customerCluster";
 
     public IReadOnlyList<RouteConfig> Routes { get; } = GenerateRoutes();
     public IReadOnlyList<ClusterConfig> Clusters { get; } = GenerateClusters();
     public IChangeToken ChangeToken { get; } = new CancellationChangeToken(cts.Token);
 
-    private static List<RouteConfig> GenerateRoutes() =>
-        new()
+    private static List<RouteConfig> GenerateRoutes()
+    {
+        var routeConfig = new RouteConfig
         {
-            new RouteConfig
+            RouteId = "customerRoute",
+            ClusterId = clusterId,
+            Match = new RouteMatch {Path = "{**catch-all}"}
+        };
+        
+        routeConfig.WithTransformPathSet("/help");
+
+        return new List<RouteConfig> {routeConfig};
+    }
+
+    private static List<ClusterConfig> GenerateClusters()
+    {
+        var clusterConfig = new ClusterConfig
+        {
+            ClusterId = clusterId,
+            Destinations = new Dictionary<string, DestinationConfig>
             {
-                RouteId = "CustomerRoute",
-                ClusterId = clusterId,
-                Match = new RouteMatch {Path = "/api/customers/{**catch-all}"}
-            }
+                {"customerServer1", new DestinationConfig {Address = "https://ya.ru"}},
+                {"customerServer2", new DestinationConfig {Address = "https://google.ru"}}
+            },
+            LoadBalancingPolicy = LoadBalancingPolicies.RoundRobin
         };
 
-    private static List<ClusterConfig> GenerateClusters() =>
-        new()
-        {
-            new ClusterConfig
-            {
-                ClusterId = clusterId,
-                Destinations = new Dictionary<string, DestinationConfig>
-                {
-                    {"customerServer1", new DestinationConfig { Address = "https://ya.ru" }},
-                    {"customerServer2", new DestinationConfig { Address = "https://google.ru" }}
-                },
-                LoadBalancingPolicy = LoadBalancingPolicies.RoundRobin
-            }
-        };
+        return new List<ClusterConfig> {clusterConfig};
+    }
 }
